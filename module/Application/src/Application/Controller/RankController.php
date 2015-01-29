33d151d4f58a7ceef7f2c6976f2e288aac8d1b0c
grisuji <grisuji@gmx.de>
2015-01-29 22:20:09 +0100

    shows spieltag-points and sort

--- module/Application/src/Application/Controller/RankController.php
+++ module/Application/src/Application/Controller/RankController.php
@@ -16,6 +16,22 @@ use Zend\Debug\Debug;
 
 class RankController  extends AbstractActionController{
 
+    private static function cmp_points($a, $b) {
+        if ($a['points'] == $b['points']) {
+            return 0;
+        } else {
+            return ($a['points'] > $b['points']) ? -1 : 1;
+        }
+    }
+
+    private static function cmp_name($a, $b) {
+        if ($a['name'] == $b['name']) {
+            return 0;
+        } else {
+            return ($a['name'] > $b['name']) ? -1 : 1;
+        }
+    }
+
     public function indexAction(){
 
         $pointhelper = new GrisujiPoints();
@@ -26,20 +42,26 @@ class RankController  extends AbstractActionController{
 
 
         $matchTable = $this->getServiceLocator()->get('MatchTable');
+        $day = $matchTable->getDayOfNextMatch();
         $matches = $matchTable->getSaisonTipsAndMatches(2014);
         $user = array();
         foreach($matches as $m) {
             if ($m->userid < 2) continue;
             if (!isset($user[$m->userid]))
             {
-                $user[$m->userid] = array('name' => $m->username, 'points' => 0);
+                $user[$m->userid] = array('name' => $m->username, 'points' => 0 , $day => 0);
             }
-            $user[$m->userid]['points'] += $pointhelper->getPoints($m->team1goals,
-                                                            $m->team2goals,
-                                                            $m->team1tip,
-                                                            $m->team2tip);
+            $points = $pointhelper->getPoints($m->team1goals,
+                $m->team2goals,
+                $m->team1tip,
+                $m->team2tip);
+
+            $user[$m->userid][$m->day] += $points;
+            $user[$m->userid]['points'] += $points;
         }
-        $view = new ViewModel(array('user'=>$user));
+        usort($user, array($this,"cmp_name"));
+        usort($user, array($this, "cmp_points"));
+        $view = new ViewModel(array('user'=>$user, 'day'=>$day));
         return $view;
     }
 }
